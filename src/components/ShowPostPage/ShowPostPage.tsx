296c3269eabfcf60c127312b8ddd337cac6f11ff
unknown <derek.slauson5@gmail.com>
2022-07-10 20:57:37 -0500

    Delete button only displays if on a post by logged in user, post is only deleted from posts context if successfully deleted from database

--- src/components/ShowPostPage/ShowPostPage.tsx
+++ src/components/ShowPostPage/ShowPostPage.tsx
@@ -1,6 +1,7 @@
 import { CircularProgress } from "@mui/material"
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
+import { useCurrentUserContext } from "../../context/CurrentUserContext"
 import { usePostsContext } from "../../context/PostsContext"
 import { PostType } from "../PostsGalleryItem/PostsGalleryItem"
 
@@ -10,6 +11,7 @@ const ShowPostPage = () => {
     const [postData, setPostData] = useState<PostType>()
     const [loadingPost, setLoadingPost] = useState(true)
     const { setPosts } = usePostsContext()
+    const { currentUser } = useCurrentUserContext()
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -19,13 +21,14 @@ const ShowPostPage = () => {
                 await fetch(`http://localhost:4000/posts/showPost/${postId}`)
             ).json()
             setPostData(fetchedPost)
+            console.log(fetchedPost)
             setLoadingPost(false)
         }
         fetchPost()
     }, [postId])
 
     const deletePost = async () => {
-        await (
+        const deletePostResponse = await (
             await fetch(`http://localhost:4000/posts/${postId}`, {
                 method: "DELETE",
                 headers: {
@@ -35,10 +38,13 @@ const ShowPostPage = () => {
             })
         ).json()
 
-        setPosts(prev => {
-            const newData = prev.data.filter(prevPost => prevPost._id !== postId)
-            return { data: [...newData], totalPosts: prev.totalPosts }
-        })
+        if (deletePostResponse.message === "Post successfully deleted") {
+            setPosts(prev => {
+                const newData = prev.data.filter(prevPost => prevPost._id !== postId)
+                return { data: [...newData], totalPosts: prev.totalPosts }
+            })
+        }
+
         navigate(-1)
     }
 
@@ -47,7 +53,8 @@ const ShowPostPage = () => {
             <p>{postData?.user.username}</p>
             <h1>{postData?.title}</h1>
             <p>{postData?.content}</p>
-            <button onClick={deletePost}>Delete</button>
+
+            {currentUser?.username === postData?.user.username && <button onClick={deletePost}>Delete</button>}
             <button onClick={() => navigate(-1)}>Back</button>
         </>
     )
